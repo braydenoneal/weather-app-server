c685cf451c780d18669e899404202b41ce46afaf
epicfacepace <boneal005@drury.edu>
2023-02-27 18:15:26 -0600

    Setup db.js

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
 // db.js
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'weather-dev',
+    password: 'weatherdev',
+    database: 'weather'
+});
+
+module.exports = connection;
